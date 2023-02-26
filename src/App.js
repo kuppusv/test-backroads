e9101873882b7516d9f4bb0187e6dffc0dd137d3
Venkatraman K <venkatraman.ece@gmail.com>
2023-02-26 16:00:25 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import Services from "./components/Services";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Tours from "./components/Tours";
-import Footer from "./components/Footer";
+import React from 'react';
+import About from './components/About';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Tours from './components/Tours';
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
       <Hero />
       <About />
       <Services />
       <Tours />
       <Footer />
-    </>
+    </React.Fragment>
   );
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 const Title = ({ title, subTitle }) => {
-    return (
-        <div className='section-title'>
-            <h2>
-                {title} <span>{subTitle}</span>
-            </h2>
-        </div>
-    );
+  return (
+    <div className='section-title'>
+      <h2>
+        {title} <span>{subTitle}</span>
+      </h2>
+    </div>
+  );
 };
 export default Title;
